6f3fdd8cdf4ac721119139777098e67296dd5c70
traaga <toomas.raaga@gmail.com>
2022-10-28 14:40:08 +0300

    Add post creating functionality from fetched data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
-//fetchPosts("https://api.jsonbin.io/v3/b/635969a30e6a79321e3565aa");
-fetchPosts("assets/js/posts.json");
+fetchPosts("https://api.jsonbin.io/v3/b/635969a30e6a79321e3565aa");
+//fetchPosts("assets/js/posts.json");
 
 function fetchPosts(url) {
     fetch(url)
@@ -16,5 +16,32 @@ function fetchPosts(url) {
 
 function createPosts(data) {
     console.log(data);
-    // todo: create posts
+    const postsParent = document.querySelector("#posts");
+
+    const postTemplate = document.querySelector("#post-template");
+    const postElement = postTemplate.content.querySelector("div");
+
+    data.posts.forEach(post => {
+        console.log(post);
+        const newPost = document.importNode(postElement, true);
+
+        const header = newPost.querySelector(".post-header");
+
+        const image = newPost.children[1];
+        const text = newPost.children[2];
+        const date = header.children[1];
+        const avatar = header.children[0];
+
+        if(post.imageUrl) {
+            image.src = post.imageUrl;
+        } else {
+            image.remove();
+        }
+
+        text.innerHTML = post.content;
+        date.innerHTML = post.date;
+        avatar.src = "assets/images/img.png";
+
+        postsParent.appendChild(newPost);
+    });
 }
