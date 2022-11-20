c1e0f2f45b08e383362844cd2980a0830ab2ca4a
Merit Laidroo <meritlaidroo@gmail.com>
2022-11-20 13:00:15 +0200

    fixed styling and moved previous work to different folder

--- webappshw3/src/components/Form.vue
+++ webappshw3/src/components/Form.vue
@@ -1,42 +1,17 @@
 <template>
-    <div id="formcontainer">
-    <form>
+  <div class="container">
+    <div class="login">
+      <form>
         <label for="email">Email</label>
         <input type="email" class="inputfield" name="email" placeholder="Email" required><br>
         <label for="password">Password</label>
         <input type="password" class="inputfield" name="password" placeholder="Password" required><br>
         <input type="submit" class="submit" name="login" value="Signup"> <br>
-    </form>
+      </form>
     </div>
+  </div>
 </template>
 
-
-<style>
-
-
-#formcontainer {
-    padding: 20px;
-    margin-top: 20px;
-    margin-bottom: 20px;
-    background-color: lightgrey;
-    border-radius: 10px;
-    width: 50%;
-    align-self: center;
-    margin-left:25%;
-}
-
-.inputfield {
-    border-radius: 10px;
-    height: 30px;
-    margin: 10px;
-    text-align: center;
-}
-
-.submit {
-    border-radius: 15px;
-    margin: 10px;
-    height: 30px;
-    width: 100px;
-    background-color:rgb(68, 160, 252);
-}
+<style >
+@import "@/assets/css/login.css";
 </style>
--- webappshw3/src/views/HomeView.vue
+++ webappshw3/src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <Header />
     <div class="container">
     <div id="left"></div>
     <div id="posts">    <h1>There will be posts here, eventually!</h1>
@@ -8,43 +7,17 @@
     <div id = "right"></div>
     </div>
     <LikeReset/>
-    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Footer from '@/components/Footer.vue'
-import Header from '@/components/Header.vue'
-import LikeReset from '@/components/LikeReset.vue';
+import LikeReset from '@/components/LikeReset';
 
 export default {
   name: 'HomeView',
   components: {
-    Footer,
-    Header,
     LikeReset,
   }
 }
 </script>
-
-<style>
-.container {
-  display: flex;
-  justify-content:space-between;
-  margin-top: 5px;
-  margin-bottom: 5px;
-}
-
-#posts {
-  width: 50%; 
-  border-radius: 10px;
-  background: grey;
-}
-
-#left, #right {
-    width: 20%;
-    background-color: black;
-    border-radius: 10px;
-}
-</style>
